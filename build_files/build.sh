c8a24e323d683a146db3024c8e773483d42cd82d
Virgile <virgile.girardin@gmail.com>
2026-03-21 14:35:47 +0100

    add and remove packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y kitty
+dnf5 remove -y powertop
 
 # Use a COPR Example:
 #
@@ -21,4 +22,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
